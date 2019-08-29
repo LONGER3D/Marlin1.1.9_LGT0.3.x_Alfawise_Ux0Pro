9a22195caba686fd481d9a7ad71fb7e78e9ef5e4
Longer3D <2059212139@qq.com>
2019-08-29 10:05:01 +0800

    Update Configuration.h
    Modify the default direction of the U30_Pro extruder motor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -874,7 +874,11 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
+#ifdef U20_Pro
 #define INVERT_E0_DIR false
+#else
+#define INVERT_E0_DIR true
+#endif
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
